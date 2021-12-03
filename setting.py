c40e7392e81d11dfe2ad1e2d0bf69b9595f9c08a
barry <friendship-119@163.com>
2021-12-03 21:10:06 +0800

    update

--- setting.py
+++ setting.py
@@ -1,7 +1,6 @@
-AWS_ACCESS_KEY_ID = 'AKIAQMS6D5EI3FOTFUE4'
+AWS_ACCESS_KEY_ID = ''
 AWS_SECRET_ACCESS_KEY = ''
-STREAM_NAME = "mock-stock-price-ds"
-STREAM2_NAME = "mock-stock-tx-ds"
+STREAM_NAME = "demo-ds"
 REGION = 'cn-northwest-1'
 # 发送间隔，毫秒数s
 SEND_INTERVAL = 1000
